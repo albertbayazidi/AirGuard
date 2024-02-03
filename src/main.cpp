3a6fd728adb5a557cf63672c7b594232504fc56e
Albert <albert.bayazidi@gmail.com>
2024-02-03 17:10:30 +0100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,199 +1,114 @@
-// include exsternal 
 #include <Arduino.h>
 #include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-// include local
-#include "http.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_AHTX0.h>
 
+#define LED 2
+int heat = 4; 
+int CO2 = 34;
 
 // Replace with your network credentials
 const char* ssid = "Fairphone";
 const char* password = "TEMPPASS";
 
-const char* PARAM_INPUT_1 = "output";
-const char* PARAM_INPUT_2 = "state";
+// Set web server port number to 80
+WiFiServer server(80);
+String header;
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
-const char index_html[] PROGMEM = R"(
-<!DOCTYPE HTML><html>
-<head>
-  <title>AirGuard</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <h2>AirGuard dashboard </h2>
-  %BUTTONPLACEHOLDER%
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
-  xhr.send();
-}
-</script>
-</body>
-</html>
-)";
-
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons = "";
-    buttons += "<h4>Output - GPIO 2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"2\" " + outputState(2) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Output - GPIO 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"4\" " + outputState(4) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Output - GPIO 33</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"33\" " + outputState(33) + "><span class=\"slider\"></span></label>";
-    return buttons;
-  }
-  return String();
-}
-
-void setup(){
-  // Serial port for debugging purposes
+void setup() {
   Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(LED,OUTPUT);
 
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-  pinMode(33, OUTPUT);
-  digitalWrite(33, LOW);
-  
-  // Connect to Wi-Fi
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+    delay(500);
+    Serial.print(".");
+  digitalWrite(LED,HIGH);
   }
-
-  // Print ESP Local IP Address
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage1;
-    String inputMessage2;
-    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
-      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
-      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
-    }
-    else {
-      inputMessage1 = "No message sent";
-      inputMessage2 = "No message sent";
-    }
-    Serial.print("GPIO: ");
-    Serial.print(inputMessage1);
-    Serial.print(" - Set to: ");
-    Serial.println(inputMessage2);
-    request->send(200, "text/plain", "OK");
-  });
-
-  // Start server
   server.begin();
 }
 
-void loop() {
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>AirGuard </h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>State DEES </p>");
+            // If the output26State is off, it displays the ON button       
+ 
+              
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
-
-
-/*
-#include <Arduino.h>
-//Local libraries
-#include "http.h"
-#include "get_vals.h"
-#include "start.h"
-// External libraries
-#include <ArduinoJson.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-
-
-String PARAM_INPUT_1 = "output";
-String PARAM_INPUT_2 = "state";
-
-// Definitions
-
-#define LED 2
-String ssid = "Fairphone";
-String password = "TEMPPASS";
-
-
-// Variables
-unsigned long lastTime = 0;
-unsigned long timerDelay = 60000; // delay is 16 hours
-JsonDocument doc;
-
-String serverName = "https://www.hvakosterstrommen.no/api/v1/prices/2024/02-02_NO3.json"; // API endpoint
-
-// DASHBOARD
-AsyncWebServer server(80);
-
-
-void setup() {
-	Serial.begin(115200);
-	startWifi(ssid, password);
-	server.begin();
-	pinMode(LED,OUTPUT);
-	digitalWrite(LED,HIGH);
-}
-
-void loop() {
-	// HTTP Request
-	HTTPClient http;
-	String serverPath = serverName;
-	http.begin(serverPath.c_str());
-	int httpResponseCode = http.GET();
-// HTTP Response and parsing
-	if (httpResponseCode>0) {
-		while ( (WiFi.status() == WL_CONNECTED) && ((millis() - lastTime) < timerDelay) ) {
-
-			digitalWrite(LED,HIGH); // WIFI indicator
-			respCode(httpResponseCode);
-
-			String payload = http.getString();
-
-			deserializeJson(doc,payload);
-
-			printVals(doc,"NOK_per_kWh");
-
-			printVals(doc,"EUR_per_kWh");
-
-			delay(10000);
-
-			}
-		}
-		else {
-			respCode(httpResponseCode);
-			delay(60000); // wait 1 minute
-		}
-		if (WiFi.status() != WL_CONNECTED) {
-			digitalWrite(LED,LOW); // WIFI indicator
-			Serial.println("WiFi Disconnected");
-		}
-		lastTime = millis();
-		http.end(); // Free resources
-
-}
-}
-*/
