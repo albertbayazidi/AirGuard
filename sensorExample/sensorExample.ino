04af5e4f8cbfaed4bcbf3530cd122d9f5f4e38bd
Iver-Iscariot <150331960+Iver-Iscariot@users.noreply.github.com>
2024-02-03 15:51:47 +0100

    Create sensorExample.ino
    hihi

---
+++ sensorExample/sensorExample.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_AHTX0.h>
+
+Adafruit_AHTX0 aht;
+
+Adafruit_BMP280 bmp; // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+int heat = 4; // monoxide heater
+
+
+void setup() {
+  Serial.begin(9600);
+  bmp.begin();
+  aht.begin();
+  
+  pinMode(heat, OUTPUT);
+}
+
+void loop() {
+
+  sensors_event_t temp_event, pressure_event;
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  
+  
+  Serial.println(temp_event.temperature);
+  Serial.println(temp.temperature);
+  Serial.println(pressure_event.pressure);
+  Serial.println(humidity.relative_humidity);
+
+  Serial.println();
+
+  
+  analogWrite(heat, 170); // set heating element i monoxide at 1.5v
+  Serial.println(analogRead(2)); // no unit what unit is, do some nice map() functions
+  Serial.println();
+  Serial.println();
+  delay(2000);
+}
